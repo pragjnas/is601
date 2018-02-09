ce5ca3cfb788b34d82c0a263f4c3df7ca1ca237d
pragjna <ps745@njit.edu>
2018-02-09 15:19:22 -0500

    Laravel setup3

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('content')
+
+        <h1>Hello world Welcome to my Aboutpage</h1>
+@stop
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('content')
+
+        <h1>Hello world Welcome to my About page</h1>
+@stop
